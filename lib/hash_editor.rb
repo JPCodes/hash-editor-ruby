9ef4095bac9bae2b06e63f0c69d7f0e9b745e998
Cody Brubaker and Jim Padilla <cody@>
2016-11-28 13:34:07 -0800

    Add test code for myfetch

--- lib/hash_editor.rb
+++ lib/hash_editor.rb
@@ -1,18 +1,40 @@
 class MyHash
-  define_method(:initialize) do |param|
-
+  define_method(:initialize) do
+    myNew()
   end
 
-  define_method(:myNew) do |param|
-
+  define_method(:myNew) do
+   @hash_array = []
   end
 
-  define_method(:myStore) do |param|
-
+  define_method(:myStore) do |key, value|
+    @hash_array.push([key, value])
   end
 
-  define_method(:myFetch) do |param|
+  define_method(:myFetch) do |key_search|
+    key_val_array = self.instance_variable_get(:@hash_array)
+
+    key_val_array.each do |param|
+      puts "first each"
+      param.each do |param_in|
+        puts "second each"
+        if param_in == key_search
+          puts "if statement"
+        end
+      end
+    end
 
   end
 
 end
+
+test_hash = MyHash.new()
+test_hash.myStore("kitten", "cute")
+test_hash.myStore("doggy", "ugly")
+test_hash.myStore("pup", "happy")
+print test_hash.instance_variable_get(:@hash_array)
+puts " "
+puts test_hash.myFetch("kitten")
+# puts test_hash.instance_of? MyHash # true
+# puts test_hash.inspect.include?("kitten") # true
+# puts test_hash.inspect.include?("cute") # true
